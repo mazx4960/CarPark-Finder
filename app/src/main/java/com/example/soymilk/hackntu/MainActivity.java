8ff85d481b723577553e6715ae4ccb8728137d90
bongbonglemon <gordonlim214@gmail.com>
2019-03-19 03:26:44 +0800

    java code

--- app/src/main/java/com/example/soymilk/hackntu/MainActivity.java
+++ app/src/main/java/com/example/soymilk/hackntu/MainActivity.java
@@ -2,12 +2,21 @@ package com.example.soymilk.hackntu;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView textView1;
+    TextView textView2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        textView1 = (TextView)findViewById(R.id.textView1);
+        textView2 = (TextView)findViewById(R.id.textView2);
+        textView1.setText("chicken");
+        textView2.setText("dog");
+
     }
 }
