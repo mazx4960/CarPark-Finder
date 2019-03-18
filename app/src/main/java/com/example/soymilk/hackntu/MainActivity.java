ca605cc8af6314172d277a0de4b83fa4afd5399e
mazx4960 <desmondtan4561@gmail.com>
2019-03-19 04:15:21 +0800

    warning: might blow minds

--- app/src/main/java/com/example/soymilk/hackntu/MainActivity.java
+++ app/src/main/java/com/example/soymilk/hackntu/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.soymilk.hackntu;
 
 import android.graphics.Color;
+import android.media.MediaPlayer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -10,6 +11,7 @@ public class MainActivity extends AppCompatActivity {
 
     TextView textView1;
     TextView textView2;
+    MediaPlayer mPlayer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,9 +19,17 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         textView1 = (TextView)findViewById(R.id.textView1);
         textView2 = (TextView)findViewById(R.id.textView2);
-        textView1.setText("chicken");
+        textView1.setText("Do not click me");
         textView2.setText("dog");
 
+        textView1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mPlayer = MediaPlayer.create(MainActivity.this, R.raw.test);
+                mPlayer.start();
+            }
+        });
     }
+
 
 }
